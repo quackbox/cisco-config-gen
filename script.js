6be7c49878dd51fcd29bef84b1ab8a35afe05fda
Xantenius <shianne.shephard@outlook.com>
2022-11-11 12:06:26 +1100

    unfinished err msgs, individual regex for all fields

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 
 const IPv4Collection = document.getElementsByClassName("ipv4");
-const newtable = Array.from(IPv4Collection);
+const TextCollection = document.getElementsByClassName("text");
 
-const ElementStyles = [];
+const newtable = Array.from(IPv4Collection);
+const textTable = Array.from(TextCollection);
+
+const minimum = 4;
+const maximum = 15;
+
+// table/object to hold the respective classes' error messages
+const errMsgs = [
+    {
+        ipv4: "Must be a valid IP Address",
+        text: "Must contain only alphanumeric characters, -, _ and be between " + minimum + " - " + maximum + " characters long with no spaces",
+        password: "Must be between " + minimum + " - " + maximum + " characters long with no spaces",
+    }
+]
 
 for (i=0; i<newtable.length; i++){
-    newtable[i].addEventListener("blur", ValidateIP);
-    ElementStyles[i] = newtable[i];
-    ElementStyles[i].color = newtable[i].style.color;
-    console.log(ElementStyles[i], newtable[i], newtable[i].style.color, ElementStyles[i].color );
+    newtable[i].addEventListener("blur", ValidateField);
 }
+
+for (i=0; i<textTable.length; i++){
+    textTable[i].addEventListener("blur", ValidateField);
+}
+
 
 function MarkError(element){
     element.target.style.color = "red";
     element.target.style.border = "2px solid red";
+    var errMsg = document.querySelector("#originalerr")
+    var errClone = errMsg.cloneNode("true")
+    errClone.classList.remove("hide")
+    console.log(element.target);
+    console.log(errMsg);
+    element.target.after(errClone)
 }
 
 function MarkFine(element){
@@ -21,14 +42,36 @@ function MarkFine(element){
     element.target.style.border = "none";
 }
 
-function ValidateIP(element){
-    const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    const match = regex.test(element.target.value);
-    console.log(match);
-    console.log(element.target.value);
+function ValidateField(element){
+    const IPRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    const textRegex = /^[a-zA-Z0-9_-]{4,15}$/;
+    const passwordRegex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{4,15}$/g;
+    // the 'type' is the first class in the class list (ipv4 or text)
+    const type = element.target.classList[0];
+
+    var match = true;
+    var regex; 
+    var err;
+
+    if (type == "ipv4"){
+        regex = IPRegex
+        err = errMsgs[0].ipv4;
+    }
+    else if (type == "password"){
+        regex = passwordRegex;
+        err = errMsgs[0].password;
+    }
+    else {
+        regex = textRegex;
+        err = errMsgs[0].text;
+    }
+
+    match = regex.test(element.target.value);
 
     if (match==false){
         MarkError(element);
+        console.log(err);
+
     }
     else {
         MarkFine(element)
@@ -53,7 +96,5 @@ function ValidateIPs(){
         console.log("cannot submit");
     }
 }
-
-console.log(newtable[0]);
 
 
